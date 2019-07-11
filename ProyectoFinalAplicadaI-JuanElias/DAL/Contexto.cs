88d8fcb5f6b422c9e6244da4f7360082743b390a
Juan Rosario <juanelias18@hotmail.com>
2019-07-11 18:16:55 -0400

    DAL con el contexto hecha

---
+++ ProyectoFinalAplicadaI-JuanElias/DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Usuarios> Usuarios { get; set; }
+
+
+        public Contexto() : base("ConStr")
+        { }
+    }
+}
