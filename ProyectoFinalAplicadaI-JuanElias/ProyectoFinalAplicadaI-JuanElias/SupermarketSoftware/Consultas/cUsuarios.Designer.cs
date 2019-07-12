15e77d746f1a1b20253c6d88f8bb6ec114d6dec0
Juan Rosario <juanelias18@hotmail.com>
2019-07-11 21:11:15 -0400

    Consulta hecha

--- ProyectoFinalAplicadaI-JuanElias/ProyectoFinalAplicadaI-JuanElias/SupermarketSoftware/Consultas/cUsuarios.Designer.cs
+++ ProyectoFinalAplicadaI-JuanElias/ProyectoFinalAplicadaI-JuanElias/SupermarketSoftware/Consultas/cUsuarios.Designer.cs
@@ -28,19 +28,164 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.DesdedateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.HastadateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.FiltrocomboBox = new System.Windows.Forms.ComboBox();
+            this.CriteriotextBox = new System.Windows.Forms.TextBox();
+            this.Consultarbutton = new System.Windows.Forms.Button();
+            this.RangocheckBox = new System.Windows.Forms.CheckBox();
+            this.ConsultadataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.ConsultadataGridView)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Desde";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(131, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hasta";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(314, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Filtro";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(511, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Criterio";
+            // 
+            // DesdedateTimePicker
+            // 
+            this.DesdedateTimePicker.CustomFormat = "dd/MM/yyyy";
+            this.DesdedateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.DesdedateTimePicker.Location = new System.Drawing.Point(15, 39);
+            this.DesdedateTimePicker.Name = "DesdedateTimePicker";
+            this.DesdedateTimePicker.Size = new System.Drawing.Size(96, 20);
+            this.DesdedateTimePicker.TabIndex = 4;
+            // 
+            // HastadateTimePicker
+            // 
+            this.HastadateTimePicker.CustomFormat = "dd/MM/yyyy";
+            this.HastadateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.HastadateTimePicker.Location = new System.Drawing.Point(134, 39);
+            this.HastadateTimePicker.Name = "HastadateTimePicker";
+            this.HastadateTimePicker.Size = new System.Drawing.Size(96, 20);
+            this.HastadateTimePicker.TabIndex = 5;
+            // 
+            // FiltrocomboBox
+            // 
+            this.FiltrocomboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.FiltrocomboBox.FormattingEnabled = true;
+            this.FiltrocomboBox.Items.AddRange(new object[] {
+            "Todo",
+            "ID",
+            "Usuario",
+            "Nombres",
+            "Email"});
+            this.FiltrocomboBox.Location = new System.Drawing.Point(317, 37);
+            this.FiltrocomboBox.Name = "FiltrocomboBox";
+            this.FiltrocomboBox.Size = new System.Drawing.Size(175, 21);
+            this.FiltrocomboBox.TabIndex = 6;
+            // 
+            // CriteriotextBox
+            // 
+            this.CriteriotextBox.Location = new System.Drawing.Point(514, 38);
+            this.CriteriotextBox.Name = "CriteriotextBox";
+            this.CriteriotextBox.Size = new System.Drawing.Size(201, 20);
+            this.CriteriotextBox.TabIndex = 7;
+            // 
+            // Consultarbutton
+            // 
+            this.Consultarbutton.Image = global::ProyectoFinalAplicadaI_JuanElias.Properties.Resources.loupe_78347;
+            this.Consultarbutton.Location = new System.Drawing.Point(721, 26);
+            this.Consultarbutton.Name = "Consultarbutton";
+            this.Consultarbutton.Size = new System.Drawing.Size(51, 41);
+            this.Consultarbutton.TabIndex = 8;
+            this.Consultarbutton.UseVisualStyleBackColor = true;
+            this.Consultarbutton.Click += new System.EventHandler(this.Consultarbutton_Click);
+            // 
+            // RangocheckBox
+            // 
+            this.RangocheckBox.AutoSize = true;
+            this.RangocheckBox.Location = new System.Drawing.Point(236, 41);
+            this.RangocheckBox.Name = "RangocheckBox";
+            this.RangocheckBox.Size = new System.Drawing.Size(75, 17);
+            this.RangocheckBox.TabIndex = 9;
+            this.RangocheckBox.Text = "Por Fecha";
+            this.RangocheckBox.UseVisualStyleBackColor = true;
+            // 
+            // ConsultadataGridView
+            // 
+            this.ConsultadataGridView.AllowUserToAddRows = false;
+            this.ConsultadataGridView.AllowUserToDeleteRows = false;
+            this.ConsultadataGridView.BackgroundColor = System.Drawing.SystemColors.Desktop;
+            this.ConsultadataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ConsultadataGridView.Location = new System.Drawing.Point(15, 74);
+            this.ConsultadataGridView.Name = "ConsultadataGridView";
+            this.ConsultadataGridView.ReadOnly = true;
+            this.ConsultadataGridView.Size = new System.Drawing.Size(757, 364);
+            this.ConsultadataGridView.TabIndex = 10;
             // 
             // cUsuarios
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(784, 450);
+            this.Controls.Add(this.ConsultadataGridView);
+            this.Controls.Add(this.RangocheckBox);
+            this.Controls.Add(this.Consultarbutton);
+            this.Controls.Add(this.CriteriotextBox);
+            this.Controls.Add(this.FiltrocomboBox);
+            this.Controls.Add(this.HastadateTimePicker);
+            this.Controls.Add(this.DesdedateTimePicker);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "cUsuarios";
             this.Text = "Consulta de Usuarios";
+            ((System.ComponentModel.ISupportInitialize)(this.ConsultadataGridView)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker DesdedateTimePicker;
+        private System.Windows.Forms.DateTimePicker HastadateTimePicker;
+        private System.Windows.Forms.ComboBox FiltrocomboBox;
+        private System.Windows.Forms.TextBox CriteriotextBox;
+        private System.Windows.Forms.Button Consultarbutton;
+        private System.Windows.Forms.CheckBox RangocheckBox;
+        private System.Windows.Forms.DataGridView ConsultadataGridView;
     }
 }
--- ProyectoFinalAplicadaI-JuanElias/ProyectoFinalAplicadaI-JuanElias/SupermarketSoftware/Consultas/cUsuarios.cs
+++ ProyectoFinalAplicadaI-JuanElias/ProyectoFinalAplicadaI-JuanElias/SupermarketSoftware/Consultas/cUsuarios.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BLL;
+using Entidades;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,136 @@ namespace ProyectoFinalAplicadaI_JuanElias.SupermarketSoftware.Consultas
         public cUsuarios()
         {
             InitializeComponent();
+        }
+
+        private void Consultarbutton_Click(object sender, EventArgs e)
+        {
+            var listado = new List<Usuarios>();
+            RepositorioBase<Usuarios> r = new RepositorioBase<Usuarios>();
+            if (RangocheckBox.Checked == true)
+            {
+                if (CriteriotextBox.Text.Trim().Length > 0)
+                {
+                    switch (FiltrocomboBox.Text)
+                    {
+                        case "Todo":
+                            listado = r.GetList(p => true);
+                            break;
+                        case "ID":
+                            int parse;
+                            if (!int.TryParse(CriteriotextBox.Text, out parse))
+                            {
+                                MessageBox.Show("Solo numeros.");
+                            }
+                            else
+                            {
+                                int id = Convert.ToInt32(CriteriotextBox.Text);
+                                listado = r.GetList(p => p.UsuarioId == id);
+                            }
+                            break;
+                        case "Nombres":
+                            if (!System.Text.RegularExpressions.Regex.IsMatch(CriteriotextBox.Text, "^[a-zA-Z ]"))
+                            {
+                                MessageBox.Show("No numeros en los nombres.");
+                            }
+                            else
+                            {
+                                listado = r.GetList(p => p.Nombres.Contains(CriteriotextBox.Text));
+                            }
+                            break;
+
+                        case "Usuario":
+                            listado = r.GetList(p => p.Usuario.Contains(CriteriotextBox.Text));
+                            break;
+                        case "Email":
+                            listado = r.GetList(p => p.Email.Contains(CriteriotextBox.Text));
+                            break;
+                    }
+                    listado = listado.Where(c => c.FechaCreacion.Date >= DesdedateTimePicker.Value.Date && c.FechaCreacion.Date <= HastadateTimePicker.Value.Date).ToList();
+                }
+                else
+                {
+                    if (FiltrocomboBox.Text == string.Empty)
+                    {
+                        MessageBox.Show("El filtro no puede estar vacio.");
+                    }
+                    else
+                       if ((string)FiltrocomboBox.Text != "Todo")
+                    {
+                        if (CriteriotextBox.Text == string.Empty)
+                        {
+                            MessageBox.Show("Al seleccionar uno de ID, Usuario, Nombres o Email necesita escribir algo en el criterio.");
+                        }
+                    }
+                    listado = r.GetList(p => true);
+                    listado = listado.Where(c => c.FechaCreacion.Date >= DesdedateTimePicker.Value.Date && c.FechaCreacion.Date <= HastadateTimePicker.Value.Date).ToList();
+                }
+                ConsultadataGridView.DataSource = null;
+                ConsultadataGridView.DataSource = listado;
+            }
+            else
+            {
+                if (CriteriotextBox.Text.Trim().Length > 0)
+                {
+                    switch (FiltrocomboBox.Text)
+                    {
+                        case "Todo":
+                            listado = r.GetList(p => true);
+                            break;
+                        case "ID":
+                            int parse;
+                            if (!int.TryParse(CriteriotextBox.Text, out parse))
+                            {
+                                MessageBox.Show("Solo numeros.");
+                            }
+                            else
+                            {
+                                int id = Convert.ToInt32(CriteriotextBox.Text);
+                                listado = r.GetList(p => p.UsuarioId == id);
+                            }
+                            break;
+                        case "Nombres":
+                            if (!System.Text.RegularExpressions.Regex.IsMatch(CriteriotextBox.Text, "^[a-zA-Z ]"))
+                            {
+                                MessageBox.Show("No numeros en los nombres.");
+                            }
+                            else
+                            {
+                                listado = r.GetList(p => p.Nombres.Contains(CriteriotextBox.Text));
+                            }
+                            break;
+
+                        case "Usuario":
+                            listado = r.GetList(p => p.Usuario.Contains(CriteriotextBox.Text));
+                            break;
+                        case "Email":
+                            listado = r.GetList(p => p.Email.Contains(CriteriotextBox.Text));
+                            break;
+                    }
+                }
+                else
+                {
+                    if (FiltrocomboBox.Text == string.Empty)
+                    {
+                        MessageBox.Show("El filtro no puede estar vacio.");
+                    }
+                    else
+                       if ((string)FiltrocomboBox.Text != "Todo")
+                    {
+                        if (CriteriotextBox.Text == string.Empty)
+                        {
+                            MessageBox.Show("Al seleccionar uno de ID, Usuario, Nombres o Email necesita escribir algo en el criterio.");
+                        }
+                    }
+                    else
+                    {
+                        listado = r.GetList(p => true);
+                    }
+
+                }
+                ConsultadataGridView.DataSource = null;
+                ConsultadataGridView.DataSource = listado;
+            }
         }
     }
 }
