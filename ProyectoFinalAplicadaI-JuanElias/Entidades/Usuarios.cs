9465fc8d5fece2c9e8d9d1c008eb7a34029f0d96
Juan Rosario <juanelias18@hotmail.com>
2019-07-17 13:42:11 -0400

    Escondiendo clave

--- ProyectoFinalAplicadaI-JuanElias/Entidades/Usuarios.cs
+++ ProyectoFinalAplicadaI-JuanElias/Entidades/Usuarios.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ namespace Entidades
         [Key]
         public int UsuarioId { get; set; }
         public string Usuario { get; set; }
+        [Browsable(false)]
         public string Clave { get; set; }
         public string Nombres { get; set; }
         public string Email { get; set; }
