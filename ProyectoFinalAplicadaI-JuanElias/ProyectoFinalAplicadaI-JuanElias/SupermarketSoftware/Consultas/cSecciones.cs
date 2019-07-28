26b501df69b2497ac1c466030805b096dc416af3
Juan Rosario <juanelias18@hotmail.com>
2019-07-27 21:57:44 -0400

    Reporte de secciones

--- ProyectoFinalAplicadaI-JuanElias/ProyectoFinalAplicadaI-JuanElias/SupermarketSoftware/Consultas/cSecciones.cs
+++ ProyectoFinalAplicadaI-JuanElias/ProyectoFinalAplicadaI-JuanElias/SupermarketSoftware/Consultas/cSecciones.cs
@@ -1,5 +1,6 @@
 ﻿using BLL;
 using Entidades;
+using ProyectoFinalAplicadaI_JuanElias.SupermarketSoftware.Reportes;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,6 +15,7 @@ namespace ProyectoFinalAplicadaI_JuanElias.SupermarketSoftware.Consultas
 {
     public partial class cSecciones : Form
     {
+        private List<Secciones> Lista;
         public cSecciones()
         {
             InitializeComponent();
@@ -70,7 +72,7 @@ namespace ProyectoFinalAplicadaI_JuanElias.SupermarketSoftware.Consultas
                     listado = r.GetList(p => true);
                     listado = listado.Where(c => c.Fecha.Date >= DesdedateTimePicker.Value.Date && c.Fecha.Date <= HastadateTimePicker.Value.Date).ToList();
                 }
-                ConsultadataGridView.DataSource = null;
+                Lista = listado;
                 ConsultadataGridView.DataSource = listado;
             }
             else
@@ -120,10 +122,23 @@ namespace ProyectoFinalAplicadaI_JuanElias.SupermarketSoftware.Consultas
                         listado = r.GetList(p => true);
                     }
                 }
-                ConsultadataGridView.DataSource = null;
+                Lista = listado;
                 ConsultadataGridView.DataSource = listado;
             }
         }
 
+        private void Imprimirbutton_Click(object sender, EventArgs e)
+        {
+            if (ConsultadataGridView.RowCount == 0)
+            {
+                MessageBox.Show("No se puede imprimir");
+                return;
+            }
+            else
+            {
+                SeccionesReport r = new SeccionesReport(Lista);
+                r.ShowDialog();
+            }
+        }
     }
 }
