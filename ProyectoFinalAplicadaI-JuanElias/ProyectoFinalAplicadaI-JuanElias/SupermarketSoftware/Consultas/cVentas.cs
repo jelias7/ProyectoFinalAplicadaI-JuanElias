62d9fc83e9e57291a975d67248706ae6b5453d6c
Juan Rosario <juanelias18@hotmail.com>
2019-07-28 01:59:51 -0400

    Reportes agregados

--- ProyectoFinalAplicadaI-JuanElias/ProyectoFinalAplicadaI-JuanElias/SupermarketSoftware/Consultas/cVentas.cs
+++ ProyectoFinalAplicadaI-JuanElias/ProyectoFinalAplicadaI-JuanElias/SupermarketSoftware/Consultas/cVentas.cs
@@ -1,5 +1,6 @@
 ﻿using BLL;
 using Entidades;
+using ProyectoFinalAplicadaI_JuanElias.SupermarketSoftware.Reportes;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,6 +15,7 @@ namespace ProyectoFinalAplicadaI_JuanElias.SupermarketSoftware.Consultas
 {
     public partial class cVentas : Form
     {
+        private List<Ventas> Lista;
         public cVentas()
         {
             InitializeComponent();
@@ -109,7 +111,7 @@ namespace ProyectoFinalAplicadaI_JuanElias.SupermarketSoftware.Consultas
                     listado = r.GetList(p => true);
                     listado = listado.Where(c => c.Fecha.Date >= DesdedateTimePicker.Value.Date && c.Fecha.Date <= HastadateTimePicker.Value.Date).ToList();
                 }
-                ConsultadataGridView.DataSource = null;
+                Lista = listado;
                 ConsultadataGridView.DataSource = listado;
             }
             else
@@ -197,9 +199,24 @@ namespace ProyectoFinalAplicadaI_JuanElias.SupermarketSoftware.Consultas
                         listado = r.GetList(p => true);
                     }
                 }
-                ConsultadataGridView.DataSource = null;
+                Lista = listado;
                 ConsultadataGridView.DataSource = listado;
             }
+        }
+
+        private void Imprimirbutton_Click(object sender, EventArgs e)
+        {
+            if (ConsultadataGridView.RowCount == 0)
+            {
+                MessageBox.Show("No se puede imprimir");
+                return;
+            }
+            else
+            {
+                VentasReport v = new VentasReport(Lista);
+                v.ShowDialog();
+            }
+
         }
     }
 }
---
+++ ProyectoFinalAplicadaI-JuanElias/ProyectoFinalAplicadaI-JuanElias/SupermarketSoftware/Reportes/VentasReport.cs
@@ -0,0 +1,32 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoFinalAplicadaI_JuanElias.SupermarketSoftware.Reportes
+{
+    public partial class VentasReport : Form
+    {
+        private List<Ventas> ListaVentas;
+        public VentasReport(List<Ventas> ventas)
+        {
+            this.ListaVentas = ventas;
+            InitializeComponent();
+        }
+
+        private void CrystalReportViewer1_Load(object sender, EventArgs e)
+        {
+            ListadoVentas listado = new ListadoVentas();
+            listado.SetDataSource(ListaVentas);
+
+            crystalReportViewer1.ReportSource = ListaVentas;
+            crystalReportViewer1.Refresh();
+        }
+    }
+}
